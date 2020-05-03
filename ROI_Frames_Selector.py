71a30df34ea00e50689931b0a929dced414adf3e
Anand Pallipurath Radhakrishnan <anandpr1602@yahoo.com>
2020-05-03 01:25:47 +0100

    Major update: Added functionality to open directories with a sequence of images and ROI Circle.
    Now users can select a directory to process all single-frame image sequences in the folder.
    
    Users can also choose a circular ROI.

--- ROI_Frames_Selector.py
+++ ROI_Frames_Selector.py
@@ -1,32 +1,36 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Apr 28 13:24:02 2020
+Created in April 2020 for UCL EIL
 
 @author: Anand Pallipurath
 """
+import os
+import configparser
 import numpy as np
 import tkinter
 import tkinter.font as font
 import tkinter.filedialog as filedialog
 import imageio
 import PIL.Image, PIL.ImageTk
-import os
-import json
 
 #  Load the configuration file
-import configparser
-config = configparser.ConfigParser()
-config.read('TKinter_ROI_Frames_Selector.cfg')
-
+ConfigParser = configparser.ConfigParser()
+ConfigParser.read('ROI_Frames_Selector.cfg')
 #  Load FFMPEG file extensions from settings
-FFMPEGFileExtensions = json.loads(config.get("SETTINGS", "FFMPEGFileExtensions"))
-
+FFMPEGFileExtensions = ConfigParser.get("SETTINGS", "FFMPEGFileExtensions")
+# Load list of single image filetypes from settings
+ImageFileExtensions = ConfigParser.get("SETTINGS", "ImageFileExtensions")
 
 class VideoBrowser:
-    def __init__(self, window, myvideo=None):
+    def __init__(self, window, myvideo=None, ROIshape=0):
         # Create a window and build the Application objects
         self.window = window
         self.myvideo = myvideo
+        if int(ROIshape) != 0 and int(ROIshape) != 1:
+            self.window.destroy()
+            raise ValueError("ROIshape must be 0 for rectangle and 1 for circle")
+        else:
+            self.ROIshape = int(ROIshape)
         self.window.title("UCL EIL - ROI and Frames Selector. Built by Anand Pallipurath.")
         
         self.resolution = 800 # Giving a decent resolution to resize large images to fit a screen
@@ -49,24 +53,45 @@ class VideoBrowser:
         self.curY = None
         self.resize = False
         self.ROIrect = False
-
-        #  Get filename file_extension
-        self.file_extension = (os.path.splitext(myvideo)[1])
+        self.filelist = []
         
-        self.image_set = imageio.get_reader(myvideo, mode = '?') #image_set is a reader object with the list of images. This loads the entire file onto memory.
+        # If a single file is selected: use the imageio.getreader() option and check for FFMPEG compatibility.
+        if os.path.isfile(myvideo) == True:
+            #  Get filename file_extension
+            self.file_extension = (os.path.splitext(myvideo)[1])
+            self.image_set = imageio.get_reader(myvideo, mode = '?') #image_set is a reader object with the list of images. This loads the entire file onto memory.
+    
+            #  Check if the imported file is a FFMPEG or some other type
+            self.isFFMPEG = self.file_extension in FFMPEGFileExtensions
+            #  Calculate the number of frames in the file. count_frames() is used for videos otherwise get_length() is used.
+            #  When reading from a video, the number of available frames is hard/expensive to calculate, which is why its
+            #  set to inf by default, indicating “stream mode”. To get the number of frames before having read them all, you
+            #  can use the reader.count_frames() method.
+            #  See: https://imageio.readthedocs.io/en/stable/format_ffmpeg.html#ffmpeg
+            self.number_frames = self.image_set.count_frames() if self.isFFMPEG else self.image_set.get_length()
+            # Create frame and photo here, only to get the aspect ratio of the photo based on which the canvas will be built.
+            self.frame = self.image_set.get_data(self.index) #get_data opens each frame as an image array
 
-        #  Check if the imported file is a FFMPEG or some other type
-        self.isFFMPEG = self.file_extension in FFMPEGFileExtensions
-
-        #  Calculate the number of frames in the file. count_frames() is used for videos otherwise get_length() is used.
-        #  When reading from a video, the number of available frames is hard/expensive to calculate, which is why its
-        #  set to inf by default, indicating “stream mode”. To get the number of frames before having read them all, you
-        #  can use the reader.count_frames() method.
-        #  See: https://imageio.readthedocs.io/en/stable/format_ffmpeg.html#ffmpeg
-        self.number_frames = self.image_set.count_frames() if self.isFFMPEG else self.image_set.get_length()
-
-        # Create frame and photo here, only to get the aspect ratio of the photo based on which the canvas will be built.
-        self.frame = self.image_set.get_data(self.index) #get_data opens each frame as an image array
+        elif os.path.isdir(myvideo) == True: # Else if a directory of image sequence is selected: use the imageio.imread() option to open frames.
+            for file in os.listdir(myvideo):
+                self.file_extension = (os.path.splitext(file)[1])
+                #  Check if the imported file is a compatible single image
+                self.isSingleImage = self.file_extension in ImageFileExtensions
+                if self.isSingleImage == True and self.file_extension != "":
+                    self.filelist.append(os.path.join(myvideo, file))
+            if self.filelist != []:
+                self.filelist = sorted(self.filelist)
+                self.number_frames = len(self.filelist)
+                self.frame = imageio.imread(self.filelist[self.index]) #open each image from directory
+            else:
+                self.mycanvas.destroy()
+                self.window.destroy()
+                raise FileNotFoundError("No suitable single-frame images found in the folder.")
+        else:
+            self.mycanvas.destroy()
+            self.window.destroy()
+            raise RuntimeError("Unknown OS error. File or Directory may be corrupted or non-existent, and couldn't be opened")
+        
         self.photo = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(self.frame), master=self.mycanvas) #convert image array into TKinter compatible image
         # Store the original aspect ratio to rescale the dataset (i.e. High-Res images will not fit the screen otherwise)
         self.original_height = self.photo.height() 
@@ -110,7 +135,11 @@ class VideoBrowser:
         self.window.mainloop()
 
     def update_canvas(self):
-        self.frame = self.image_set.get_data(self.index) # get_data opens each frame as an image array
+        if os.path.isfile(self.myvideo) == True:
+            self.frame = self.image_set.get_data(self.index) # get_data opens each frame as an image array
+        elif os.path.isdir(self.myvideo) == True:
+            self.frame = imageio.imread(self.filelist[self.index]) #open each image from directory
+
         # Convert image array into TKinter compatible image. master=self.mycanvas tells Tkinter to make the photo available to mycanvas and NOT the window (which is a separate Tkinter.Tk() instance)
         self.photo = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(self.frame), master=self.mycanvas) 
         # Resize the photo if needed
@@ -120,7 +149,7 @@ class VideoBrowser:
             self.resize = True
             self.photo = PIL.ImageTk.PhotoImage(image = (PIL.Image.fromarray(self.frame)).resize((int(np.round(self.photo.width()*self.resolution/self.original_height)), self.resolution)), master=self.mycanvas)
             self.mycanvas = tkinter.Canvas(self.window, width = self.photo.width(), height = self.photo.height(), highlightthickness=0)
-        # Post the photo onto the canvas and NOT the window. Create a tag for the photo on the canvas to later handle mouse events occurring ONLY on the photo and not on other objects drawn on mycanvas (e.g. the ROI rectangle)
+        # Post the photo onto the canvas and NOT the window. Create a tag for the photo on the canvas to later handle mouse events occurring ONLY on the photo and not on other objects drawn on mycanvas (e.g. the ROI rectangle or Circle)
         self.mycanvas.create_image(0, 0, image = self.photo, anchor = tkinter.NW, tags="mypic")
         self.mycanvas.config(scrollregion=self.mycanvas.bbox('mypic'))
         self.mycanvas.grid(row = 1, column = 0, columnspan = 3)
@@ -247,7 +276,7 @@ class VideoBrowser:
         self.mycanvas.bind('<Motion>', self.motion)
     
     def drawROI(self):
-        #If to re-draw a rectangle, delete all previous objects on mycanvas and start afresh.
+        #If to re-draw the rectangle or circle, delete all previous objects on mycanvas and start afresh.
         if self.ROIrect == True:
             self.mycanvas.delete('all')
             self.rect = None
@@ -273,22 +302,25 @@ class VideoBrowser:
             self.myxy = tkinter.Label(self.window, text = "XY coordinates: " + str(np.round(self.x*self.original_height/self.resolution)) + ", " + str(np.round(self.y*self.original_height/self.resolution)))
         self.myxy.grid(row = 0, column = 2, columnspan = 1)
     
-    # Create a rectangle on left-mouse click ONLY if there are no other rectangles already present.
+    # Create a rectangle or a circle on left-mouse click ONLY if there are no other rectangles or circles already present.
     def on_button_press(self, event1):
-        if not self.rect: # create rectangle if not yet exist
+        if not self.rect: # create rectangle/ circle if not yet exist
             # save mouse drag start position
             self.start_x = event1.x
             self.start_y = event1.y
-            self.rect = self.mycanvas.create_rectangle(self.x, self.y, 1, 1, outline='red')
+            if self.ROIshape == 0:
+                self.rect = self.mycanvas.create_rectangle(self.x, self.y, self.x+1, self.y+1, outline='red')
+            else:
+                self.rect = self.mycanvas.create_oval(self.x, self.y, self.x+1, self.y+1, outline='red')
 
-    # Update the rectangle size as the mouse performs 'move press' ONLY if the use has clicked 'draw ROI' or 'reselect ROI'
+    # Update the rectangle/circle size as the mouse performs 'move press' ONLY if the use has clicked 'draw ROI' or 'reselect ROI'
     def on_move_press(self, event2):
         if self.myROI_button['state'] == "disabled":
             self.curX = event2.x
             self.curY = event2.y
-            # expand rectangle as you drag the mouse
+            # expand rectangle/circle as you drag the mouse
             self.mycanvas.coords(self.rect, self.start_x, self.start_y, self.curX, self.curY)
-    
+
     def on_button_release(self, event3):
         self.myROI_button.grid_forget()
         self.update_ROI()
@@ -348,26 +380,73 @@ class VideoBrowser:
                 self.start_y = int(np.round(self.start_y*self.original_height/self.resolution))
                 self.curX = int(np.round(self.curX*self.original_height/self.resolution))
                 self.curY = int(np.round(self.curY*self.original_height/self.resolution))
-                print("Image rescaled to fit screen. Possible rounding error of pixel coordinates!")
-            print("ROI Rectangle ( X1, Y1, X2, Y2 ): (", self.start_x, ",", self.start_y, ",", self.curX, ",", self.curY, ")")
+                print("Image rescaled to fit screen. Rounding error of pixel coordinates possible!")
+            if self.ROIshape == 0:
+                print("ROI Rectangle ( X1, Y1, X2, Y2 ): (", self.start_x, ",", self.start_y, ",", self.curX, ",", self.curY, ")")
+            else:
+                print("ROI Circle Bounding Box ( X1, Y1, X2, Y2 ): (", self.start_x, ",", self.start_y, ",", self.curX, ",", self.curY, ")")
         else:
-            print("ROI Rectangle not selected. 'None' type will be returned!")
+            print("ROI not selected. 'None' type will be returned!")
         print()
-        self.image_set.close()
+        if os.path.isfile(self.myvideo) == True:
+            self.image_set.close()
         self.mycanvas.destroy()
         self.firstframe_button.destroy()
         self.lastframe_button.destroy()
         self.myROI_button.destroy()
         self.window.destroy()
 
-if __name__ == "__main__":
-    root = tkinter.Tk()
+def opt1_select():
     root.withdraw()
     root.filename = filedialog.askopenfilename(title = "Select a File", filetypes = (("All Files", "*.*"),))
     filename = root.filename
     root.destroy()
     if filename != "":
-        output = VideoBrowser(tkinter.Tk(), filename).results()
-        print(output)
+        VideoBrowser(tkinter.Tk(), filename, root.roishape.get())
     else:
         print("No file selected.")
+
+def opt2_select():
+    root.withdraw()
+    root.filedir = filedialog.askdirectory(title = "Select a Folder", mustexist = True)
+    filedir = root.filedir + "/"
+    root.destroy()
+    if filedir != "":
+        VideoBrowser(tkinter.Tk(), filedir, root.roishape.get())
+    else:
+        print("No folder selected.")
+
+#Code starts here                       
+if __name__ == "__main__":  
+    root = tkinter.Tk()
+    root.title("UCL EIL - ROI and Frames Selector. Built by Anand Pallipurath.")
+    root.roishape = tkinter.IntVar()
+    
+    root.frame3 = tkinter.Radiobutton(root, text = "Rectangle", variable = root.roishape, value = 0)
+    root.frame3.grid(row = 1, column = 0, columnspan = 1)
+    
+    root.frame4 = tkinter.Radiobutton(root, text = "Circle", variable = root.roishape, value = 1)
+    root.frame4.grid(row = 1, column = 2, columnspan = 1)
+    
+    root.frame3a = tkinter.Label(root, text = "Specify the shape of the ROI", state = "active")
+    root.frame3a['font'] = font.Font(family="Helvetica", size=10, weight=font.BOLD, slant=font.ITALIC)
+    root.frame3a.grid(row = 0, column = 1, columnspan = 1)
+    
+    root.frame1 = tkinter.Button(root, text = "Open Single Multimedia File", command = opt1_select, state = "active")
+    root.frame1['font'] = font.Font(family="Helvetica", size=10, weight=font.BOLD, slant=font.ITALIC)
+    root.frame1.grid(row = 2, column = 0, columnspan = 1)
+    
+    root.frame1a = tkinter.Label(root, text = "(e.g.: TIFF, JPEG, AVI, MP4, etc.)", state = "active")
+    root.frame1a.grid(row = 3, column = 0, columnspan = 1)
+    
+    root.frame2 = tkinter.Button(root, text = "Open Multiple Image Files from a Sequence", command = opt2_select, state = "active")
+    root.frame2['font'] = font.Font(family="Helvetica", size=10, weight=font.BOLD, slant=font.ITALIC)
+    root.frame2.grid(row = 2, column = 2, columnspan = 1)
+    
+    root.frame2a = tkinter.Label(root, text = "(e.g.: A Sequence of TIFF files in a Folder)", state = "active")
+    root.frame2a.grid(row = 3, column = 2, columnspan = 1)
+    
+    root.frame4 = tkinter.Button(root, text = "CANCEL", command = root.destroy, state = "active")
+    root.frame4['font'] = font.Font(family="Helvetica", size=10, weight=font.BOLD, slant=font.ITALIC)
+    root.frame4.grid(row = 4, column = 1, columnspan = 1)
+    root.mainloop()
